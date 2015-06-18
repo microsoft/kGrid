0901703a8be4614ca55baf8dd7698085eae9af8a
Bin Du <bidu@microsoft.com>
2015-06-18 15:35:49 +0800

    Change dir structure

--- src/scripts/head.p.ts
+++ src/scripts/head.p.ts
@@ -1,3 +1,40 @@
+/**
+ * @license MIT
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Microsoft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/**
+ * Microsoft module
+ * @namespace Microsoft
+ */
+/**
+ * Microsoft.Office module
+ * @namespace Microsoft.Office
+ */
+/**
+ * Microsoft.Office.Controls module
+ * @namespace Microsoft.Office.Controls
+ */
+
 // import $ = require('jquery');
 // import angular = require('angular');
 // import css = require('css!assets/css/listcontrol.css');
